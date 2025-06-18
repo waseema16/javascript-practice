b7b029213fcc600926a421f77d13de82441246c6
waseema16 <waseemabca2025@gmail.com>
2025-06-18 21:20:37 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,50 @@
+let a = 5;
+let b = 10;
+// arithmetic operator
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a*b =",a*b);
+console.log("a/b =",a/b);
+console.log("a%b =",a%b);
+console.log("a**b =",a**b);
+a+=4;
+a=a+4;
+// assignment operator
+console.log(a);
+console.log("a+b =",a+=b);
+console.log("a-b =",a=a+b);
+console.log("a*b =",a*=b);
+console.log("a/b =",a/=b);
+console.log("a%b =",a%=b);
+console.log("a**b =",a**=b);
+a = 5;
+a++;
+// post increment oprator
+console.log("increment operator",a)
+b = 3;
+++b;
+// pre increment oprator
+console.log("increment operator",b)
+a = 5;
+a--;
+// post decrement
+console.log("decrement operator",a)
+b = 3;
+--b;
+// pre decrement
+console.log("decrement operator",b)
+a = 's';b ='n';
+// comparison operator
+console.log("equal to",a == b);
+a = '5';b ='5';
+console.log("equal to",a === b);
+console.log("equal to",a != b);
+console.log("equal to",a !== b);
+console.log("equal to",a > b);
+console.log("equal to",a < b);
+console.log("equal to",a >= b);
+console.log("equal to",a <= b);
+// logical oprator
+console.log(0 || "Hello");/*and*/
+console.log("hi" && 42);/*or*/
+console.log(! "");/*not*/
